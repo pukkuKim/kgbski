c365121ad203c3d7edb7744402b9e3d4264f908a
Ji Young Kim <goldie.kim89@gmail.com>
2024-10-14 21:46:05 -0700

    /home path removed

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./css/index.css";
 import App from "./components/App";
-import Hero from "./components/Hero";
+// import Hero from "./components/Hero";
 import "semantic-ui-css/semantic.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<Router>
+	<Router basename="/kgbski">
 		<Routes>
-			<Route path="/" element={<Hero />} />
-			<Route path="/home" element={<App />} />
+			{/* <Route path="/" element={<Hero />} /> */}
+			<Route path="/" element={<App />} />
 		</Routes>
 	</Router>
 );
